6eeecf2e74e81cf3a02b3430f531bf88775ce9bd
maocl023 <maocl023@gmail.com>
2016-04-27 12:55:57 +0800

    add video loaded progress

--- JMPlayer/Views/JMPlayerView.m
+++ JMPlayer/Views/JMPlayerView.m
@@ -12,7 +12,9 @@
 
 static int JMPlayerViewKVOContext = 0;
 
-@interface JMPlayerView ()
+@interface JMPlayerView () {
+    id _timeObserverToken;
+}
 
 @property (nonatomic, copy) NSMutableArray<AVPlayerItem *> *playerItems;
 
@@ -21,6 +23,8 @@ static int JMPlayerViewKVOContext = 0;
 @property (nonatomic) AVPlayerLayer *playerLayer;
 
 @property (nonatomic) UISlider *slider;
+
+@property (nonatomic) UIProgressView *progressView;
 
 @property (nonatomic) CMTime currentTime;
 
@@ -36,10 +40,21 @@ static int JMPlayerViewKVOContext = 0;
 
     if (self) {
         _URLs = URLs.copy;
+        self.backgroundColor = [UIColor blackColor];
         [self _setupPlayer];
     }
 
     return self;
+}
+
+- (void)layoutSubviews {
+    _progressView.center = _slider.center;
+
+    [super layoutSubviews];
+}
+
+- (void)dealloc {
+    [self _removePlayerObserver];
 }
 
 #pragma mark - Public
@@ -60,23 +75,27 @@ static int JMPlayerViewKVOContext = 0;
         return;
     }
 
-    if ([keyPath isEqualToString:@"currentItem.duration"]) {
-        NSValue *newDurationAsValue = change[NSKeyValueChangeNewKey];
-        CMTime newDuration = [newDurationAsValue isKindOfClass:[NSValue class]] ? newDurationAsValue.CMTimeValue : kCMTimeZero;
-        BOOL hasValidDuration = CMTIME_IS_NUMERIC(newDuration) && newDuration.value != 0;
-        double newDurationSeconds = hasValidDuration ? CMTimeGetSeconds(newDuration) : 0.0;
+    if ([keyPath isEqualToString:@"player.currentItem.duration"]) {
+        CMTime duration = _player.currentItem.duration;
+        BOOL isValidDuration = CMTIME_IS_NUMERIC(duration) && duration.value != 0;
+        double durationSecond = isValidDuration ? CMTimeGetSeconds(duration) : 0.0;
 
-        self.slider.maximumValue = newDurationSeconds;
-        self.slider.value = hasValidDuration ? CMTimeGetSeconds(self.currentTime) : 0.0;
-        self.slider.enabled = hasValidDuration;
-    }
-//    } else if ([keyPath isEqualToString:@"currentItem.loadedTimeRanges"]) {
-//        NSLog(@"1");
-//    } else if ([keyPath isEqualToString:@"currentItem.playbackBufferEmpty"]) {
-//        NSLog(@"2");
-//    } else if ([keyPath isEqualToString:@"currentItem.playbackLikelyToKeepUp"]) {
-//        NSLog(@"3");
-//    }
+        self.slider.maximumValue = durationSecond;
+        self.slider.value = isValidDuration ? CMTimeGetSeconds(self.currentTime) : 0.0;
+        self.slider.enabled = isValidDuration;
+    } else if ([keyPath isEqualToString:@"player.currentItem.loadedTimeRanges"]) {
+        NSArray *loadedTimeRages = _player.currentItem.loadedTimeRanges;
+        CMTimeRange timeRage = [(NSValue *)loadedTimeRages[0] CMTimeRangeValue];
+        double start = CMTimeGetSeconds(timeRage.start);
+        double duration = CMTimeGetSeconds(timeRage.duration);
+        double loaded = start + duration;
+
+        self.progressView.progress = loaded / CMTimeGetSeconds(_player.currentItem.duration);
+    }// else if ([keyPath isEqualToString:@"currentItem.playbackBufferEmpty"]) {
+    //        NSLog(@"2");
+    //    } else if ([keyPath isEqualToString:@"currentItem.playbackLikelyToKeepUp"]) {
+    //        NSLog(@"3");
+    //    }
 }
 
 - (void)sliderValueChanged:(UISlider *)slider {
@@ -95,12 +114,27 @@ static int JMPlayerViewKVOContext = 0;
         _slider = [[UISlider alloc] initWithFrame:(CGRect){
             (CGPoint){self.frame.origin.x, self.frame.origin.y + 24.f},
             (CGSize){self.frame.size.width, 24.f}}];
+        _slider.minimumTrackTintColor = [UIColor redColor];
+        _slider.maximumTrackTintColor = [UIColor colorWithRed:0.3 green:0.3 blue:0.3 alpha:0.6];
+
         [_slider addTarget:self
                     action:@selector(sliderValueChanged:)
           forControlEvents:UIControlEventValueChanged];
     }
 
     return _slider;
+}
+
+- (UIProgressView *)progressView {
+    if (!_progressView) {
+        _progressView = [[UIProgressView alloc] initWithFrame:(CGRect){
+            (CGPoint){self.frame.origin.x + 2.f, self.frame.origin.y + 24.f},
+            (CGSize){self.frame.size.width - 4.f, 24.f}}];
+        _progressView.progressTintColor = [UIColor colorWithRed:1 green:1 blue:1 alpha:0.3];
+        _progressView.trackTintColor = [UIColor clearColor];
+    }
+
+    return _progressView;
 }
 
 - (CMTime)currentTime {
@@ -113,17 +147,17 @@ static int JMPlayerViewKVOContext = 0;
         toleranceBefore:kCMTimeZero
          toleranceAfter:kCMTimeZero
       completionHandler:^(BOOL finished)
-    {
-        @strongify(self)
+     {
+         @strongify(self)
 
-        if (finished) {
-            // just continue playing
-            [self play];
-        } else {
-            // pause while sliding
-            [self pause];
-        }
-    }];
+         if (finished) {
+             // just continue playing
+             [self play];
+         } else {
+             // pause while sliding
+             [self pause];
+         }
+     }];
 }
 
 #pragma mark - Private
@@ -145,36 +179,50 @@ static int JMPlayerViewKVOContext = 0;
 
     [self.layer addSublayer:_playerLayer];
 
+    [self addSubview:self.progressView];
     [self addSubview:self.slider];
 
     [self _addPlayerObserver];
 }
 
 - (void)_addPlayerObserver {
-    [_player addObserver:self
-              forKeyPath:@"currentItem.duration"
-                 options:NSKeyValueObservingOptionNew | NSKeyValueObservingOptionInitial
-                 context:&JMPlayerViewKVOContext];
-//    [_player addObserver:self
-//              forKeyPath:@"currentItem.loadedTimeRanges"
-//                 options:NSKeyValueObservingOptionNew// | NSKeyValueObservingOptionInitial
-//                 context:&JMPlayerViewKVOContext];
-//    [_player addObserver:self
-//              forKeyPath:@"currentItem.playbackBufferEmpty"
-//                 options:NSKeyValueObservingOptionNew //| NSKeyValueObservingOptionInitial
-//                 context:&JMPlayerViewKVOContext];
-//    [_player addObserver:self
-//              forKeyPath:@"currentItem.playbackLikelyToKeepUp"
-//                 options:NSKeyValueObservingOptionNew// | NSKeyValueObservingOptionInitial
-//                 context:&JMPlayerViewKVOContext];
+    [self addObserver:self
+           forKeyPath:@"player.currentItem.duration"
+              options:NSKeyValueObservingOptionNew | NSKeyValueObservingOptionInitial
+              context:&JMPlayerViewKVOContext];
+    [self addObserver:self
+           forKeyPath:@"player.currentItem.loadedTimeRanges"
+              options:NSKeyValueObservingOptionNew// | NSKeyValueObservingOptionInitial
+              context:&JMPlayerViewKVOContext];
+    //    [_player addObserver:self
+    //              forKeyPath:@"currentItem.playbackBufferEmpty"
+    //                 options:NSKeyValueObservingOptionNew //| NSKeyValueObservingOptionInitial
+    //                 context:&JMPlayerViewKVOContext];
+    //    [_player addObserver:self
+    //              forKeyPath:@"currentItem.playbackLikelyToKeepUp"
+    //                 options:NSKeyValueObservingOptionNew// | NSKeyValueObservingOptionInitial
+    //                 context:&JMPlayerViewKVOContext];
     @weakify(self)
-    [_player addPeriodicTimeObserverForInterval:CMTimeMake(1, 1)
-                                          queue:NULL
-                                     usingBlock:^(CMTime time)
-    {
-        @strongify(self)
-        self.slider.value = CMTimeGetSeconds(time);
-    }];
+    _timeObserverToken = [_player addPeriodicTimeObserverForInterval:CMTimeMake(1, 1)
+                                                               queue:NULL
+                                                          usingBlock:^(CMTime time)
+                          {
+                              @strongify(self)
+                              self.slider.value = CMTimeGetSeconds(time);
+                          }];
+}
+
+- (void)_removePlayerObserver {
+    if (_timeObserverToken) {
+        [_player removeTimeObserver:_timeObserverToken];
+    }
+
+    [self removeObserver:self
+              forKeyPath:@"player.currentItem.duration"
+                 context:&JMPlayerViewKVOContext];
+    [self removeObserver:self
+              forKeyPath:@"player.currentItem.loadedTimeRanges"
+                 context:&JMPlayerViewKVOContext];
 }
 
 @end
